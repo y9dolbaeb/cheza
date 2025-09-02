12f766e2a237233e01c92d2291d0965da159d6f6
y9dolbaeb <temuujinshine@gmail.com>
2025-09-02 12:58:19 +0800

    chanhges

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,12 +20,14 @@ export default function Home() {
       <main className="max-w-4xl mx-auto px-4 py-16 flex flex-col md:flex-row items-center">
         <div className="md:w-1/2">
           <h1 className="text-3xl font-bold mb-4 text-indigo-600">Привет, я Тупой аутист</h1>
-          <p className="mb-4 text-black">Я фронтенд разработчик. Создаю простые и современные веб-приложения с использованием Next.js и Tailwind CSS.</p>
-          <a href="mailto:youremail@example.com" className="text-white bg-indigo-600 px-4 py-2 rounded">Связаться</a>
+          <p className="mb-4 text-black">Я просто ахуенный челик который освоил на ебанутом уровне такие программы по типу <span className="text-orange-500 font-bold">Illustrator </span> 
+  & <span className="text-indigo-500 font-bold">Photoshop </span> а также Figma также я ахуенно пишу код на <span className="text-orange-500 font-bold">HTML </span> 
+  & <span className="text-indigo-500 font-bold">CSS </span></p>
+          <a href="mailto:youremail@example.com" className="text-white bg-indigo-600 px-4 py-2 rounded">О</a>
         </div>
 
         <div className="md:w-1/2 mt-8 md:mt-0 md:ml-8">
-          <img src="https://i.pinimg.com/474x/4e/79/d8/4e79d8b711edc3022fc8e4dcdf9d12a5.jpg./" alt="Моё фото" width={250} height={250} className="rounded-xl" />
+          <img src="./man.png" alt="Моё фото" width={450} height={450} className="rounded-xl" />
         </div>
       </main>
 
